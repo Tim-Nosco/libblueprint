8eb1fd9657a67fabab9ba8cfe0fe8a7156156746
Nosco <EVOFORGE\tmnosco@a-2uol28r7e893e.evoforge.org>
2019-01-15 15:16:08 -0500

    Add call to zlib and base64.
    Code compiles, but unittest results in segfault. Data type?

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,36 @@
 //A blueprint string is a JSON representation of the blueprint, compressed with zlib deflate and then encoded using base64 with a version byte in front. The version byte is currently 0 for vanilla 0.15 and 0.16. So to get the JSON representation of a blueprint from a blueprint string, skip the first byte, base64 decode the string, and finally decompress using zlib inflate.
 
 use base64;
-use compress::zlib;
+use libflate::zlib;
 use json;
+use std::io::Read;
+
 
 #[no_mangle]
 pub extern fn decode(_input:String)->String {
-	//Skip the first byte 
+	//Skip the first byte, b64 decode
+	println!("DEBUG: decoding input");
 	let _raw = base64::decode(&_input[1..]).unwrap();
-	// let _dec = 
-	return String::from("0");
+	//zlib decompress
+	println!("DEBUG: initializing decoder");
+	let mut decompressed = String::new();
+	decompressed = match &mut zlib::Decoder::new(&*_raw) {
+		Ok(decoder) => {
+			match decoder.read_to_string(&mut decompressed) {
+				Ok(_len) => decompressed,
+				Err(error) => {
+					println!("Unable to read data.\n{}", error);
+					String::from("0")
+				},
+			}
+		},
+		Err(error) => {
+			println!("Unable to initialize Decoder.\n{}", error);
+			String::from("0")
+		},
+	};
+
+	return decompressed;
 }
 
 #[no_mangle]
