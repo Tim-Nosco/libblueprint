b6430be6c8df3727802922c92319a53d13dff30a
Nosco <EVOFORGE\tmnosco@a-2uol28r7e893e.evoforge.org>
2019-01-18 11:26:27 -0500

    sync corpus

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 docker build -t bp .
-docker run -it --rm --privileged bp $@
+
+docker run -it --rm -e "HFUZZ_INPUT=/corpus/$1" --privileged \
+-v $PWD/libblueprint/test/hfuzz/corpus:/corpus bp $1
