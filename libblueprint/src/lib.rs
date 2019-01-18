c28c76207ebc2e49e3674810489741a835f68f8a
Nosco <EVOFORGE\tmnosco@a-2uol28r7e893e.evoforge.org>
2019-01-18 12:43:07 -0500

    fixed crashing erros

--- libblueprint/src/lib.rs
+++ libblueprint/src/lib.rs
@@ -7,28 +7,66 @@ use std::ffi::{CString,CStr};
 use libflate::zlib;
 use std::io::Read;
 
-#[no_mangle]
-pub extern fn decode(s: *const c_char) -> *mut c_char {
+
+use std::fmt;  
+use std::error::Error as StdError;
+type Result<T> = std::result::Result<T, Box<StdError>>;
+
+#[derive(Debug)]
+pub enum Error {  
+    NullArgument,
+	StringTooShort,
+}
+impl fmt::Display for Error {  
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::NullArgument => f.write_str("NullArgument"),
+            Error::StringTooShort => f.write_str("StringTooShort"),
+        }
+    }
+}
+impl StdError for Error {  
+    fn description(&self) -> &str {
+        match *self {
+            Error::NullArgument => "Null argument provided to function.",
+            Error::StringTooShort => "The blueprint spec requires a string of length greater than 1.",
+        }
+    }
+}
+
+fn decode_helper(s: *const c_char) -> Result<*mut c_char> {
 	//interpret c string
     let c_str = unsafe {
-        assert!(!s.is_null());
+        if s.is_null() {
+			return Err(Error::NullArgument.into());
+		}
         CStr::from_ptr(s)
     };
-    let r_str = c_str.to_str().unwrap();
-	//decode the string
-	println!("DEBUG: Read string: {:?}", r_str);
-	//spec says skip the first byte
-	if r_str.chars().count() <= 1 {
-		return CString::new("None").unwrap().into_raw();
+    let r_str = c_str.to_bytes();
+	println!("DEBUG: Found string: {:?}", r_str);
+	//decode the string. spec says to skip the first byte
+	if r_str.len() <= 1 {
+		return Err(Error::StringTooShort.into());
 	}
-	let raw = base64::decode(&r_str[1..]).unwrap();
+	let raw = base64::decode(&r_str[1..])?;
 	//decompress
-	let mut decoder = zlib::Decoder::new(&raw[..]).unwrap();
+	let mut decoder = zlib::Decoder::new(&raw[..])?;
 	let mut deflated = Vec::new();
-	decoder.read_to_end(&mut deflated).unwrap();
-	let deflated = String::from_utf8(deflated).unwrap();
+	decoder.read_to_end(&mut deflated)?;
+	let deflated = String::from_utf8(deflated)?;
 	//print results
 	println!("DEBUG: Decoded data: {:?}", deflated);
-	let deflated = CString::new(deflated).unwrap();
-	return deflated.into_raw();
+	let deflated = CString::new(deflated)?;
+	return Ok(deflated.into_raw());
+}
+
+#[no_mangle]
+pub extern fn decode(s: *const c_char) -> *mut c_char {
+	match decode_helper(s) {
+		Ok(e) => e,
+		Err(e) => {
+			println!("WARNING: {}", e);
+			CString::new("None").unwrap().into_raw()
+		}
+	}
 }
--- libblueprint/test/hfuzz/src/decode.rs
+++ libblueprint/test/hfuzz/src/decode.rs
@@ -5,20 +5,11 @@ use blueprint::decode;
 use std::ffi::CString;
 
 fn main() {
-    // Here you can parse `std::env::args and 
-    // setup / initialize your project
-
-    // You have full control over the loop but
-    // you're supposed to call `fuzz` ad vitam aeternam
     loop {
-        // The fuzz macro gives an arbitrary object (see `arbitrary crate`)
-        // to a closure-like block of code.
-        // For performance reasons, it is recommended that you use the native type
-        // `&[u8]` when possible.
-        // Here, this slice will contain a "random" quantity of "random" data.
         fuzz!(|data: &[u8]| {
-            let s = CString::new(data).unwrap();
-            let s = s.into_raw();
+            let s = unsafe {
+                CString::from_vec_unchecked(data.to_vec()).into_raw()
+            };
             decode(s);
         });
     }
