c7fc96ce6c6f60b97e8e171904d0eb0780182fb3
Nosco <EVOFORGE\tmnosco@a-2uol28r7e893e.evoforge.org>
2019-01-19 14:33:45 -0500

    example test for json_to_grid

--- libblueprint/test/hfuzz/src/decode.rs
+++ libblueprint/test/hfuzz/src/decode.rs
@@ -10,14 +10,10 @@ fn main() {
         fuzz!(|data: &[u8]| {
             //call export1
             let ret = decode_to_json(data.as_ptr(), data.len());
-            let ret_array = unsafe {
-                CStr::from_ptr(ret as *const i8).to_bytes()
-            };
+
             //call export2
-            let ret2 = decode_to_grid(
-                ret_array.as_ptr(), 
-                ret_array.len()
-            );
+            let ret2 = decode_to_grid(data.as_ptr(), data.len());
+            
             //let go of returned memory
             blueprint::free_return(ret);
             blueprint::free_return(ret2);            
